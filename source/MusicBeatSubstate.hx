cfb2cd60afbc2f3f0644a9fecd2b60aa962094a9
idktrolled <camilocarlos1100@gmail.com>
2026-05-12 21:26:09 -0500

    Update MusicBeatSubstate.hx

--- source/MusicBeatSubstate.hx
+++ source/MusicBeatSubstate.hx
@@ -3,6 +3,9 @@ package;
 import Conductor.BPMChangeEvent;
 import flixel.FlxG;
 import flixel.FlxSubState;
+#if mobile
+import mobile.VirtualPad;
+#end
 
 class MusicBeatSubstate extends FlxSubState
 {
@@ -17,6 +20,20 @@ class MusicBeatSubstate extends FlxSubState
 	private var curStep:Int = 0;
 	private var curBeat:Int = 0;
 	private var controls(get, never):Controls;
+
+	#if mobile
+	var virtualPad:VirtualPad;
+	var camStill:FlxCamera;
+
+	public function addVirtualPad(?DPad:FlxDPadMode, ?Action:FlxActionMode) {
+		camStill = new FlxCamera();
+		FlxG.cameras.add(camStill);
+		camStill.bgColor.alpha = 0;
+		virtualPad = new VirtualPad(DPad, Action);
+		virtualPad.cameras = [camStill];
+		add(virtualPad);
+	}
+	#end
 
 	inline function get_controls():Controls
 		return PlayerSettings.player1.controls;
