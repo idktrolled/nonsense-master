0197b6f87e431b27d7390c4e9f37b82fc767cb4e
idktrolled <camilocarlos1100@gmail.com>
2026-05-12 22:38:11 -0500

    Update VirtualPad.hx

--- source/mobile/VirtualPad.hx
+++ source/mobile/VirtualPad.hx
@@ -155,7 +155,7 @@ class VirtualPad extends FlxSpriteGroup {
 		button.immovable = true;
 		button.scrollFactor.set();
 		button.color = Color;
-		button.alpha = 0.6;
+		button.alpha = 0.65;
 		button.antialiasing = true;
 
 		return button;
