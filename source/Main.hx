59399aa557e6cb78ef7bf973486ed4b9ee0f7568
idktrolled <camilocarlos1100@gmail.com>
2026-05-13 12:47:34 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -122,7 +122,7 @@ class Main extends Sprite
 
 	public function setFPSCap(cap:Float)
 	{
-		openfl.Lib.current.stage.frameRate = 120;
+		openfl.Lib.current.stage.frameRate = cap;
 	}
 
 	public function getFPSCap():Float
